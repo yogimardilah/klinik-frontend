67daf7a6b3073b1c1f0cd0ed79077a44d9f586a1
yogimardilah <yogimardilah82@gmail.com>
2025-06-19 18:35:49 +0700

    fix: dynamic base config for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/klinik-frontend/' : '/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  const isVercel = process.env.VERCEL === '1' // variabel env default di Vercel
+  const base = isVercel ? '/' : '/klinik-frontend/'
+
+  return {
+    base,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   }
-}))
+})
