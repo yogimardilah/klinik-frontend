d1b2afcd6ae9059f10e00fcbf04cac5b4148b9f8
yogimardilah <yogimardilah82@gmail.com>
2025-06-19 18:23:22 +0700

    fix: set dynamic base path for GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/klinik-frontend/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
-})
+}))
